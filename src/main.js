fe9d8427f44cb317db073d258bb19d51a90ac310
shujin <shujin.jiang@greatopensource.com>
2024-06-24 00:29:26 +0800

    update login.vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue' // 引入 createApp 方法
 import App from '@/App.vue'     // 引入 App.vue 组件
 import '@/assets/main.css'      // 引入 main.css 样式文件
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'animate.css'
 
 // 导入路由
 import router from '@/router'
 
 const app = createApp(App)
 
+
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 // 应用路由
 app.use(router)
