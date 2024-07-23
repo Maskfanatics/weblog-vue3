86dde347d88f9cf69e6da363082ff79243607937
shujin <shujin.jiang@greatopensource.com>
2024-07-23 23:22:45 +0800

    feat: category manage

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <RouterView></RouterView>
+  <!-- 设置语言为中文 -->
+  <el-config-provider :locale="locale">
+     <router-view></router-view>
+  </el-config-provider>
 </template>
 
 <script setup>
-	
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const locale = zhCn
 </script>
 
 <style>
